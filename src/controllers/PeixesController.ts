742d4efda776720965809f3c34377a707ba427e6
Charles Magno <magnocharles286@gmail.com>
2025-12-23 11:10:34 -0300

    Charles: FEATURE - Services

--- src/controllers/PeixesController.ts
+++ src/controllers/PeixesController.ts
@@ -2,14 +2,17 @@ import { ipcMain } from 'electron'
 import { PeixeModel } from '../models/PeixeModel'
 import DatabaseConstructor from 'better-sqlite3'
 import { NewPeixe } from '../shared/types/interfaces'
+import { PeixeService } from '../services/peixeService'
 
 export class PeixesController {
   private model: PeixeModel
+  private service: PeixeService
   private db: InstanceType<typeof DatabaseConstructor>
 
   constructor(db: InstanceType<typeof DatabaseConstructor>) {
     this.db = db
     this.model = new PeixeModel(db)
+    this.service = new PeixeService(this.model)
     this.registrarRotas()
   }
 
@@ -17,7 +20,7 @@ export class PeixesController {
     ipcMain.handle('listarPeixes', async () => {
       try {
         // console.log(this.model.listar())
-        return this.model.listar()
+        return this.service.listar()
       } catch (error) {
         console.error('Erro ao listar membros:', error)
         throw error
@@ -28,7 +31,7 @@ export class PeixesController {
       try {
         return {
           success: true,
-          data: this.model.add(doc)
+          data: this.service.adicionar(doc)
         }
       } catch (error) {
         console.error('Erro ao adicionar novo grupo:', error)
@@ -38,7 +41,7 @@ export class PeixesController {
 
     ipcMain.handle('listarPeixeById', (event, id) => {
       try {
-        return this.model.listarById(id)
+        return this.service.listarById(id)
       } catch (error) {
         console.error('Erro ao buscar peixe', error)
         throw error
@@ -47,7 +50,7 @@ export class PeixesController {
 
     ipcMain.handle('deletarPeixe', (event, id) => {
       try {
-        return this.model.delete(id)
+        return this.service.delete(id)
       } catch (error) {
         console.error('Erro ao deletar peixe', error)
         throw error
