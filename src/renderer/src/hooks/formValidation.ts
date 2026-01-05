51889a4c6f529f1d30c330416941d4826f072a40
ti810 <ti@una.ba.gov.br>
2026-01-05 08:04:26 -0300

    mudanças temporarias

---
+++ src/renderer/src/hooks/formValidation.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+
+export function formValidation<T>(
+  schema: z.ZodType<T>,
+  data: unknown
+) /*{ success: true; data: T } | { success: false; errors: string[] }*/ {
+  const result = schema.safeParse(data)
+
+  if (!result.success) {
+    return {
+      success: false,
+      fieldErrors: result.error.flatten().fieldErrors ?? {}
+      // errors: result.error.issues.map((err) => err.message),
+      // errors: result.error.flatten().fieldErrors
+    }
+  }
+
+  return {
+    success: true as const,
+    data: result.data
+  }
+}
+
+export const grupoSchema = z.object({
+  nome: z
+    .string()
+    .trim()
+    .min(4, 'Nome do Grupo está muito curto')
+    .max(50, 'Nome do Grupo é muito longo'),
+  qtde_membros: z.number().min(2, 'Quantidade de Membros por Grupos no minimo 2')
+})
